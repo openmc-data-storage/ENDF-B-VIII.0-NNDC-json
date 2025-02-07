0c627a7215dab12ab243d0e80d105eddfbc5674a
shimwell <mail@jshimwell.com>
2025-02-08 00:11:00 +0100

    adding index

--- convert_json.py
+++ convert_json.py
@@ -3,8 +3,8 @@ import openmc_data_to_json as odj
 from pathlib import Path
 
 odj.cross_section_h5_files_to_json_files(
-    filenames = list(Path('nuclear_data/endfb-viii.0-hdf5/neutron').glob('*.h5')),
-    output_dir = 'ENDF-B-VIII.0_json',
+    filenames = list(Path('/nuclear_data/neutron').glob('*.h5')),
+    output_dir = 'json_files',
     library='ENDFB-8.0',
     index_filename='ENDFB-8.0_index.json',
     indent=None,
